37619eedddad4a9a3926856ea68dcf12d0a06133
rowdyslav <rowdyslav@gmail.com>
2024-10-02 18:26:14 +0300

    dns

--- database/loader.py
+++ database/loader.py
@@ -1,6 +1,9 @@
+import dns.resolver
 from environs import Env
 from motor.motor_tornado import MotorClient
 
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ["8.8.8.8"]
 env = Env()
 env.read_env()
 
